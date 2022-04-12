81176938e7ada5e1a1d79636283639b87b946811
Tim Van Wassenhove <tim@timvw.be>
2022-04-12 09:52:23 +0200

    leverage tokio-stream

--- src/myserver.rs
+++ src/myserver.rs
@@ -3,6 +3,7 @@ use crate::arrow_flight_protocol::flight_service_server::FlightService;
 use crate::arrow_flight_protocol::{Action, Criteria, Empty, FlightData, FlightDescriptor, FlightInfo, HandshakeRequest, HandshakeResponse, SchemaResult, Ticket, PutResult, Result as ActionResult, ActionType};
 use futures_core::Stream;
 use std::pin::Pin;
+use tokio_stream::wrappers::ReceiverStream;
 
 #[derive(Debug)]
 pub struct MyServer {}
@@ -17,13 +18,13 @@ impl MyServer {
 #[tonic::async_trait]
 impl FlightService for MyServer {
 
-    type HandshakeStream = Pin<Box<dyn Stream<Item = Result<HandshakeResponse, Status>> + Send + Sync + 'static>>;
+    type HandshakeStream = ReceiverStream<Result<HandshakeResponse, Status>>;
 
     async fn handshake(&self, request: Request<Streaming<HandshakeRequest>>) -> Result<Response<Self::HandshakeStream>, Status> {
         todo!()
     }
 
-    type ListFlightsStream = Pin<Box<dyn Stream<Item = Result<FlightInfo, Status>> + Send + Sync + 'static>>;
+    type ListFlightsStream = ReceiverStream<Result<FlightInfo, Status>>;
 
     async fn list_flights(&self, request: Request<Criteria>) -> Result<Response<Self::ListFlightsStream>, Status> {
         todo!()
@@ -37,31 +38,31 @@ impl FlightService for MyServer {
         todo!()
     }
 
-    type DoGetStream = Pin<Box<dyn Stream<Item = Result<FlightData, Status>> + Send + Sync + 'static>>;
+    type DoGetStream = ReceiverStream<Result<FlightData, Status>>;
 
     async fn do_get(&self, request: Request<Ticket>) -> Result<Response<Self::DoGetStream>, Status> {
         todo!()
     }
 
-    type DoPutStream = Pin<Box<dyn Stream<Item = Result<PutResult, Status>> + Send + Sync + 'static>>;
+    type DoPutStream = ReceiverStream<Result<PutResult, Status>>;
 
     async fn do_put(&self, request: Request<Streaming<FlightData>>) -> Result<Response<Self::DoPutStream>, Status> {
         todo!()
     }
 
-    type DoExchangeStream = Pin<Box<dyn Stream<Item = Result<FlightData, Status>> + Send + Sync + 'static>>;
+    type DoExchangeStream = ReceiverStream<Result<FlightData, Status>>;
 
     async fn do_exchange(&self, request: Request<Streaming<FlightData>>) -> Result<Response<Self::DoExchangeStream>, Status> {
         todo!()
     }
 
-    type DoActionStream = Pin<Box<dyn Stream<Item = Result<ActionResult, Status>> + Send + Sync + 'static>>;
+    type DoActionStream = ReceiverStream<Result<ActionResult, Status>>;
 
     async fn do_action(&self, request: Request<Action>) -> Result<Response<Self::DoActionStream>, Status> {
         todo!()
     }
 
-    type ListActionsStream = Pin<Box<dyn Stream<Item = Result<ActionType, Status>> + Send + Sync + 'static>>;
+    type ListActionsStream = ReceiverStream<Result<ActionType, Status>>;
 
     async fn list_actions(&self, request: Request<Empty>) -> Result<Response<Self::ListActionsStream>, Status> {
         todo!()
