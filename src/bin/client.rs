317499cefd634b52e43678fee93ad0f415f09bc3
Tim Van Wassenhove <tim@timvw.be>
2022-04-12 09:12:45 +0200

    adapt code to follow approach in https://github.com/hyperium/tonic/blob/master/examples/routeguide-tutorial.md

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -6,12 +6,14 @@ use arrow_flightsql_odbc::arrow_flight_protocol::Empty;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut client = FlightServiceClient::connect("http://localhost:50051").await?;
-    let actionsResponse = client.list_actions(Empty {}).await?;
-    let mut actions = actionsResponse.into_inner();
 
-    while let Some(at) = actions.next().await {
-        let at = at?;
-        println!("action type: {} desc: {}", at.r#type, at.description);
+    let mut action_types = client
+        .list_actions(Empty {})
+        .await?
+        .into_inner();
+
+    while let Some(action_type) = action_types.message().await? {
+        println!("action type: {} desc: {}", action_type.r#type, action_type.description);
     }
 
     Ok(())
