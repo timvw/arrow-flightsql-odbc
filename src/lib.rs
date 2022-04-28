e3e763bb1993b561f629e1e5ad9ceae20d30251f
Tim Van Wassenhove <tim@timvw.be>
2022-04-28 15:04:32 +0200

    make some impl details not public

--- src/lib.rs
+++ src/lib.rs
@@ -18,8 +18,8 @@ pub mod arrow_flight_protocol_sql;
 pub const FLIGHT_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("flight_descriptor");
 pub const FLIGHT_SQL_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("flight_sql_descriptor");
 
-pub mod myserver;
-
 pub mod error;
-pub mod odbc_command_handler;
+mod util;
+mod odbc_command_handler;
 mod flight_sql_command;
+pub mod myserver;
